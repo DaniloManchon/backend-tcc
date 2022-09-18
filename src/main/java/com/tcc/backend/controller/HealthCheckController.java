e395757a106efdb15a8e82a16adf541a830ca4a3
Danilo Manchon dos Santos <danilomanchon@gmail.com>
2022-09-18 11:12:04 -0300

    refactor properties

--- src/main/java/com/tcc/backend/controller/HealthCheckController.java
+++ src/main/java/com/tcc/backend/controller/HealthCheckController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
     @GetMapping("/health")
-    public ResponseEntity<String> healthCheck(){
+    public ResponseEntity<String> healthCheck() {
         return new ResponseEntity<>("Online", HttpStatus.OK);
     }
 }
